00591d3bf25cabc96e59e79fe5c83c3c7238f14d
Gustavo <140917585+Guzitos@users.noreply.github.com>
2025-11-19 20:38:14 -0300

    Initial commit

---
+++ src/test/java/com/loop/APIuser/ApIuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loop.APIuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
